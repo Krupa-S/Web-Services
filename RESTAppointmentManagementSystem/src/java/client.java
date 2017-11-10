7bec5b90578621c909cdead7aef1fc67197bf237
shah Krupa <32451066+Krupa-S@users.noreply.github.com>
2017-11-10 01:02:41 -0500

    Update client.java

--- RESTAppointmentManagementSystem/src/java/client.java
+++ RESTAppointmentManagementSystem/src/java/client.java
@@ -6,12 +6,6 @@ import java.time.Clock;
 import javax.xml.bind.JAXBException;
 import service.AppointmentService;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author krupaShah
@@ -23,33 +17,20 @@ public class client {
         AppointmentService service = new AppointmentService();
         System.out.println(service.getInfo());
         
-        // Print out all appointments
-//        System.out.println("All appointments\n");
-//        for (Object obj : objs){
-//            System.out.println(((Appointment)obj).getPatientid() + "\n");
-//        }
-        
+       
         String xmlString = "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"?>\n" +
-"<appointment>\n" +
-"    <date>2017-05-18</date>\n" +
-"    <time>10:00</time>\n" +
-"    <patientId>210</patientId>\n" +
-"    <physicianId>10</physicianId>\n" +
-"    <pscId>500</pscId>\n" +
-"    <phlebotomistId>1000</phlebotomistId>\n" +
-"    <labTests>\n" +
-"        <test id=\"86900\" dxcode=\"292.9\" />\n" +
-"    </labTests>\n" +
-"</appointment>";
+                            "<appointment>\n" +
+                            "    <date>2017-05-18</date>\n" +
+                            "    <time>10:00</time>\n" +
+                            "    <patientId>210</patientId>\n" +
+                            "    <physicianId>10</physicianId>\n" +
+                            "    <pscId>500</pscId>\n" +
+                            "    <phlebotomistId>1000</phlebotomistId>\n" +
+                            "    <labTests>\n" +
+                            "        <test id=\"86900\" dxcode=\"292.9\" />\n" +
+                            "    </labTests>\n" +
+                            "</appointment>";
 
-        //System.out.println(service.getAtllAppointments());
-        //System.out.println(service.getAppointment("700"));
-       System.out.println(service.updateAppointment(xmlString,"700"));
-        //System.out.println(service.getAppointment("700"));
-        
-        
-      // System.out.println(service.createAppointment(createXML));
-      //System.out.println(service.getAppointment("700"));
-      //System.out.println(service.getAtllAppointments());
+        System.out.println(service.updateAppointment(xmlString,"700"));
     }
 }
